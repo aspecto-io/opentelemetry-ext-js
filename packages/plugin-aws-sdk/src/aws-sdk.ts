45e2c6f29806c1485e8bc3bd4ebd129d05c42240
Amir Blum <amir@aspecto.io>
2020-08-05 11:04:09 +0300

    chore(opentelemetry-plugin-aws-sdk): bump version to v0.0.7

--- packages/plugin-aws-sdk/src/aws-sdk.ts
+++ packages/plugin-aws-sdk/src/aws-sdk.ts
@@ -16,7 +16,7 @@ import { AttributeNames } from "./enums";
 import { ServicesExtensions } from "./services";
 import { AwsSdkPluginConfig } from "./types";
 
-const VERSION = "0.0.6";
+const VERSION = "0.0.7";
 
 class AwsPlugin extends BasePlugin<typeof AWS> {
   readonly component: string;
