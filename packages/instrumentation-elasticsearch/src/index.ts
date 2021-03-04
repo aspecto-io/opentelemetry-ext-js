08625e2ab795fc0a5a74205329f1b057ae7070b5
Yaniv Davidi <yaniv.davi@gmail.com>
2021-03-04 15:06:35 +0200

    feat: new elasticsearch instrumentation (#70)

---
+++ packages/instrumentation-elasticsearch/src/index.ts
@@ -0,0 +1,2 @@
+export * from './elasticsearch';
+export * from './types';
---
+++ packages/instrumentation-elasticsearch/src/types.ts
@@ -0,0 +1,29 @@
+import { Span } from '@opentelemetry/api';
+import { InstrumentationConfig } from '@opentelemetry/instrumentation';
+
+export type DbStatementSerializer = (operation?: string, params?: object, options?: object) => string;
+
+export type ElasticsearchResponseCustomAttributesFunction = (span: Span, response: any) => void;
+
+export interface ElasticsearchInstrumentationConfig extends InstrumentationConfig {
+    /**
+     * Elasticsearch operation use http/https under the hood.
+     * If Elasticsearch instrumentation is enabled, an http/https operation will also create.
+     * Setting the `suppressInternalInstrumentation` config value to `true` will
+     * cause the instrumentation to suppress instrumentation of underlying operations,
+     * effectively causing http/https spans to be non-recordable.
+     */
+    suppressInternalInstrumentation?: boolean;
+
+    /** Custom serializer function for the db.statement tag */
+    dbStatementSerializer?: DbStatementSerializer;
+
+    /** hook for adding custom attributes using the response payload */
+    responseHook?: ElasticsearchResponseCustomAttributesFunction;
+
+    /**
+     * If passed, a span attribute will be added to all spans with key of the provided "moduleVersionAttributeName"
+     * and value of the module version.
+     */
+    moduleVersionAttributeName?: string;
+}
