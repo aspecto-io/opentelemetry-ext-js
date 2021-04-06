cf54651e2fd30118ba1fc8567ebdd1ba577e515f
Amir Blum <amir@aspecto.io>
2021-04-06 17:42:04 +0300

    chore(aws-sdk): set env variables to try and fix tests

--- packages/instrumentation-aws-sdk/test/aws-sdk-v3.spec.ts
+++ packages/instrumentation-aws-sdk/test/aws-sdk-v3.spec.ts
@@ -1,3 +1,7 @@
+// set aws environment variables, so tests are able to run
+process.env.AWS_ACCESS_KEY_ID = 'testing';
+process.env.AWS_SECRET_ACCESS_KEY = 'testing';
+
 import 'mocha';
 import { AwsInstrumentation, NormalizedRequest, NormalizedResponse } from '../src';
 import { InMemorySpanExporter, ReadableSpan, SimpleSpanProcessor, Span } from '@opentelemetry/tracing';
