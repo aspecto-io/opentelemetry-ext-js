bd15d3577547872213215404a43fda60edc44313
motti <motti@aspecto.io>
2021-05-18 12:00:38 +0300

    modernize plugin code

--- packages/instrumentation-socket.io/src/socket.io.ts
+++ packages/instrumentation-socket.io/src/socket.io.ts
@@ -7,12 +7,12 @@ import {
     safeExecuteInTheMiddle,
 } from '@opentelemetry/instrumentation';
 import { SemanticAttributes } from '@opentelemetry/semantic-conventions';
-import { SocketIoInstrumentationConfig, io } from './types';
+import { SocketIoInstrumentationConfig, Io } from './types';
 import { VERSION } from './version';
 
 const reservedEvents = ['connect', 'connect_error', 'disconnect', 'disconnecting', 'newListener', 'removeListener'];
 
-export class SocketIoInstrumentation extends InstrumentationBase<io> {
+export class SocketIoInstrumentation extends InstrumentationBase<Io> {
     protected _config!: SocketIoInstrumentationConfig;
 
     constructor(config: SocketIoInstrumentationConfig = {}) {
@@ -28,172 +28,176 @@ export class SocketIoInstrumentation extends InstrumentationBase<io> {
         const strictEventEmitterInstrumentation = new InstrumentationNodeModuleFile<any>(
             'socket.io/dist/typed-events.js',
             ['*'],
-            this.patchStrictEventEmitter.bind(this),
-            this.unpatchStrictEventEmitter.bind(this)
+            (moduleExports) => {
+                if (moduleExports === undefined || moduleExports === null) {
+                    return moduleExports;
+                }
+                diag.debug(`applying patch to socket.io StrictEventEmitter`);
+                if (isWrapped(moduleExports?.StrictEventEmitter?.prototype?.on)) {
+                    this._unwrap(moduleExports.StrictEventEmitter.prototype, 'on');
+                }
+                this._wrap(moduleExports.StrictEventEmitter.prototype, 'on', this._patchOn());
+                return moduleExports;
+            },
+            (moduleExports) => {
+                if (isWrapped(moduleExports?.StrictEventEmitter?.prototype?.on)) {
+                    this._unwrap(moduleExports.StrictEventEmitter.prototype, 'on');
+                }
+                return moduleExports;
+            }
         );
 
         const broadcastOperatorInstrumentation = new InstrumentationNodeModuleFile<any>(
             'socket.io/dist/broadcast-operator.js',
             ['*'],
-            this.patchBroadcastOperator.bind(this),
-            this.unpatchBroadcastOperator.bind(this)
+            (moduleExports) => {
+                if (moduleExports === undefined || moduleExports === null) {
+                    return moduleExports;
+                }
+                diag.debug(`applying patch to socket.io StrictEventEmitter`);
+                if (isWrapped(moduleExports?.BroadcastOperator?.prototype?.emit)) {
+                    this._unwrap(moduleExports.BroadcastOperator.prototype, 'emit');
+                }
+                this._wrap(moduleExports.BroadcastOperator.prototype, 'emit', this._patchEmit());
+                return moduleExports;
+            },
+            (moduleExports) => {
+                if (isWrapped(moduleExports?.BroadcastOperator?.prototype?.emit)) {
+                    this._unwrap(moduleExports.BroadcastOperator.prototype, 'emit');
+                }
+                return moduleExports;
+            }
         );
 
-        const module = new InstrumentationNodeModuleDefinition<io>(
+        const module = new InstrumentationNodeModuleDefinition<Io>(
             'socket.io',
             ['*'],
-            this.patch.bind(this),
-            this.unpatch.bind(this),
+            (moduleExports) => {
+                if (moduleExports === undefined || moduleExports === null) {
+                    return moduleExports;
+                }
+                diag.debug(`applying patch to socket.io`);
+                if (isWrapped(moduleExports.Server.prototype.emit)) {
+                    this._unwrap(moduleExports.Server.prototype, 'emit');
+                }
+                this._wrap(moduleExports.Server.prototype, 'emit', this._patchEmit());
+                if (isWrapped(moduleExports.Socket.prototype.emit)) {
+                    this._unwrap(moduleExports.Socket.prototype, 'emit');
+                }
+                this._wrap(moduleExports.Socket.prototype, 'emit', this._patchEmit());
+                if (isWrapped(moduleExports.Namespace.prototype.emit)) {
+                    this._unwrap(moduleExports.Namespace.prototype, 'emit');
+                }
+                this._wrap(moduleExports.Namespace.prototype, 'emit', this._patchEmit());
+                return moduleExports;
+            },
+            (moduleExports) => {
+                if (isWrapped(moduleExports.Server.prototype.emit)) {
+                    this._unwrap(moduleExports.Server.prototype, 'emit');
+                }
+                if (isWrapped(moduleExports.Socket.prototype.emit)) {
+                    this._unwrap(moduleExports.Socket.prototype, 'emit');
+                }
+                if (isWrapped(moduleExports.Namespace.prototype.emit)) {
+                    this._unwrap(moduleExports.Namespace.prototype, 'emit');
+                }
+            },
             [strictEventEmitterInstrumentation, broadcastOperatorInstrumentation]
         );
         return module;
     }
 
-    protected patchStrictEventEmitter(moduleExports: any) {
-        if (moduleExports === undefined || moduleExports === null) {
-            return moduleExports;
-        }
-        diag.debug(`applying patch to socket.io StrictEventEmitter`);
-        this.unpatchStrictEventEmitter(moduleExports);
-        this._wrap(moduleExports.StrictEventEmitter.prototype, 'on', this._createOnPatch.bind(this));
-        return moduleExports;
-    }
-
-    protected unpatchStrictEventEmitter(moduleExports: any) {
-        if (isWrapped(moduleExports?.StrictEventEmitter?.prototype?.on)) {
-            this._unwrap(moduleExports.StrictEventEmitter.prototype, 'on');
-        }
-        return moduleExports;
-    }
-
-    protected patchBroadcastOperator(moduleExports: any) {
-        if (moduleExports === undefined || moduleExports === null) {
-            return moduleExports;
-        }
-        diag.debug(`applying patch to socket.io StrictEventEmitter`);
-        this.unpatchBroadcastOperator(moduleExports);
-        this._wrap(moduleExports.BroadcastOperator.prototype, 'emit', this._createEmitPatch.bind(this));
-        return moduleExports;
-    }
-
-    protected unpatchBroadcastOperator(moduleExports: any) {
-        if (isWrapped(moduleExports?.BroadcastOperator?.prototype?.emit)) {
-            this._unwrap(moduleExports.BroadcastOperator.prototype, 'emit');
-        }
-        return moduleExports;
-    }
-
-    protected patch(moduleExports: io, moduleVersion: string) {
-        if (moduleExports === undefined || moduleExports === null) {
-            return moduleExports;
-        }
-        diag.debug(`applying patch to socket.io`);
-        this.unpatch(moduleExports);
-        this._wrap(moduleExports.Server.prototype, 'emit', this._createEmitPatch.bind(this));
-        this._wrap(moduleExports.Socket.prototype, 'emit', this._createEmitPatch.bind(this));
-        this._wrap(moduleExports.Namespace.prototype, 'emit', this._createEmitPatch.bind(this));
-        return moduleExports;
-    }
-
-    protected unpatch(moduleExports: io): void {
-        if (isWrapped(moduleExports.Server.prototype.emit)) {
-            this._unwrap(moduleExports.Server.prototype, 'emit');
-        }
-        if (isWrapped(moduleExports.Socket.prototype.emit)) {
-            this._unwrap(moduleExports.Socket.prototype, 'emit');
-        }
-        if (isWrapped(moduleExports.Namespace.prototype.emit)) {
-            this._unwrap(moduleExports.Namespace.prototype, 'emit');
-        }
-    }
-
-    private _createOnPatch(original: Function) {
+    private _patchOn() {
         const self = this;
-        return function (ev: any, originalListener: Function) {
-            if (!self._config.traceReserved && reservedEvents.includes(ev)) {
-                return original.apply(this, arguments);
-            }
-            const wrappedListener = function () {
-                const operation = 'on';
-                const messageName = ev;
+        return (original: Function) => {
+            return function (ev: any, originalListener: Function) {
+                if (!self._config.traceReserved && reservedEvents.includes(ev)) {
+                    return original.apply(this, arguments);
+                }
+                const wrappedListener = function () {
+                    const operation = 'on';
+                    const messageName = ev;
 
+                    const attributes = {
+                        [SemanticAttributes.MESSAGING_SYSTEM]: 'socket.io',
+                        [SemanticAttributes.MESSAGING_DESTINATION]: originalListener.name,
+                        [SemanticAttributes.MESSAGING_OPERATION]: operation,
+                    };
+                    const span: Span = self.tracer.startSpan(`socket.io ${operation} ${messageName}`, {
+                        kind: SpanKind.PRODUCER,
+                        attributes,
+                    });
+
+                    if (self._config.onHook) {
+                        safeExecuteInTheMiddle(
+                            () => self._config.onHook(span, arguments),
+                            (e) => {
+                                if (e) diag.error(`opentelemetry.socket.io instrumentation: OnHook error`, e);
+                            },
+                            true
+                        );
+                    }
+
+                    const result = context.with(setSpan(context.active(), span), () =>
+                        originalListener.apply(this, arguments)
+                    );
+                    span.end();
+                    return result;
+                };
+
+                return original.apply(this, [ev, wrappedListener]);
+            };
+        };
+    }
+
+    private _patchEmit() {
+        const self = this;
+        return (original: Function) => {
+            return function (ev: any, ...args: any[]): boolean {
+                if (!self._config.traceReserved && reservedEvents.includes(ev)) {
+                    return original.apply(this, arguments);
+                }
+                const operation = 'emit';
+                const messageName = ev;
                 const attributes = {
                     [SemanticAttributes.MESSAGING_SYSTEM]: 'socket.io',
-                    [SemanticAttributes.MESSAGING_DESTINATION]: originalListener.name,
+                    [SemanticAttributes.MESSAGING_DESTINATION_KIND]: 'topic',
                     [SemanticAttributes.MESSAGING_OPERATION]: operation,
                 };
-                const span: Span = self.tracer.startSpan(`socket.io ${operation} ${messageName}`, {
+
+                if (this.rooms && this.rooms.size > 0) {
+                    const rooms = Array.from<string>(this.rooms);
+                    attributes['rooms'] = rooms.join();
+                }
+
+                if (this.name) {
+                    attributes['namespace'] = this.name;
+                }
+
+                const span = self.tracer.startSpan(`socket.io ${operation} ${messageName}`, {
                     kind: SpanKind.PRODUCER,
                     attributes,
                 });
 
-                if (self._config.onHook) {
+                if (self._config.emitHook) {
                     safeExecuteInTheMiddle(
-                        () => self._config.onHook(span, arguments),
+                        () => self._config.emitHook(span, args),
                         (e) => {
-                            if (e) diag.error(`opentelemetry.socket.io instrumentation: OnHook error`, e);
+                            if (e) diag.error(`opentelemetry.socket.io instrumentation: emitHook error`, e);
                         },
                         true
                     );
                 }
-
-                const result = context.with(setSpan(context.active(), span), () =>
-                    originalListener.apply(this, arguments)
-                );
+                const succuss = context.with(setSpan(context.active(), span), () => original.apply(this, arguments));
+                if (!succuss) {
+                    span.setStatus({
+                        code: SpanStatusCode.ERROR,
+                        message: '',
+                    });
+                }
                 span.end();
-                return result;
+                return succuss;
             };
-
-            return original.apply(this, [ev, wrappedListener]);
-        };
-    }
-
-    private _createEmitPatch(original: Function) {
-        const self = this;
-        return function (ev: any, ...args: any[]): boolean {
-            if (!self._config.traceReserved && reservedEvents.includes(ev)) {
-                return original.apply(this, arguments);
-            }
-            const operation = 'emit';
-            const messageName = ev;
-            const attributes = {
-                [SemanticAttributes.MESSAGING_SYSTEM]: 'socket.io',
-                [SemanticAttributes.MESSAGING_DESTINATION_KIND]: 'topic',
-                [SemanticAttributes.MESSAGING_OPERATION]: operation,
-            };
-
-            if (this.rooms && this.rooms.size > 0) {
-                const rooms = Array.from<string>(this.rooms);
-                attributes['rooms'] = rooms.join();
-            }
-
-            if (this.name) {
-                attributes['namespace'] = this.name;
-            }
-
-            const span = self.tracer.startSpan(`socket.io ${operation} ${messageName}`, {
-                kind: SpanKind.PRODUCER,
-                attributes,
-            });
-
-            if (self._config.emitHook) {
-                safeExecuteInTheMiddle(
-                    () => self._config.emitHook(span, args),
-                    (e) => {
-                        if (e) diag.error(`opentelemetry.socket.io instrumentation: emitHook error`, e);
-                    },
-                    true
-                );
-            }
-            const succuss = context.with(setSpan(context.active(), span), () => original.apply(this, arguments));
-            if (!succuss) {
-                span.setStatus({
-                    code: SpanStatusCode.ERROR,
-                    message: '',
-                });
-            }
-            span.end();
-            return succuss;
         };
     }
 }
--- packages/instrumentation-socket.io/src/types.ts
+++ packages/instrumentation-socket.io/src/types.ts
@@ -1,6 +1,6 @@
 import { Span } from '@opentelemetry/api';
 import { InstrumentationConfig } from '@opentelemetry/instrumentation';
-import type * as socketIo from 'socket.io';
+import type * as io from 'socket.io';
 
 export type TransportInstrumentationConfig = {
     httpInstrumentation: any;
@@ -11,11 +11,11 @@ export type OnHook = (span: Span, args: any) => void;
 export interface SocketIoInstrumentationConfig extends InstrumentationConfig {
     /** Hook for adding custom attributes before socket.io emits the event */
     emitHook?: EmitHook;
-    /** Hook for adding custom attributes before socket.io calls an event listener (callback) */
+    /** Hook for adding custom attributes before the event listener (callback) is invoked */
     onHook?: OnHook;
-    /** Set to true if you want to trace socket.io reserved events */
+    /** Set to true if you want to trace socket.io reserved events (see https://socket.io/docs/v4/emit-cheatsheet/#Reserved-events) */
     traceReserved?: boolean;
     /** Set to TransportInstrumentationConfig if you want to filter out socket.io HTTP transport */
     filterTransport?: false | TransportInstrumentationConfig;
 }
-export type io = typeof socketIo;
+export type Io = typeof io;
