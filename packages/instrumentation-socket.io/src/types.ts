0088e563211a458b51bfd61bf6be631c5d3bcda3
motti <motti@aspecto.io>
2021-05-19 09:50:01 +0300

    use interfaces for config

--- packages/instrumentation-socket.io/src/types.ts
+++ packages/instrumentation-socket.io/src/types.ts
@@ -6,13 +6,18 @@ export type TransportInstrumentationConfig = {
     httpInstrumentation: any;
     socketPath: string;
 };
-export type EmitHook = (span: Span, args: any) => void;
-export type OnHook = (span: Span, args: any) => void;
+export interface SocketIoHookInfo {
+    args: any[];
+}
+
+export interface SocketIoHookFunction {
+    (span: Span, hookInfo: SocketIoHookInfo): void;
+}
 export interface SocketIoInstrumentationConfig extends InstrumentationConfig {
     /** Hook for adding custom attributes before socket.io emits the event */
-    emitHook?: EmitHook;
+    emitHook?: SocketIoHookFunction;
     /** Hook for adding custom attributes before the event listener (callback) is invoked */
-    onHook?: OnHook;
+    onHook?: SocketIoHookFunction;
     /** Set to true if you want to trace socket.io reserved events (see https://socket.io/docs/v4/emit-cheatsheet/#Reserved-events) */
     traceReserved?: boolean;
     /** Set to TransportInstrumentationConfig if you want to filter out socket.io HTTP transport */
