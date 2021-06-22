944da660443a28cd065b22594d888728b67c4a27
Amir Blum <amir@aspecto.io>
2021-06-22 21:27:31 +0300

    chore: add missing file from previous commit

---
+++ detectors/node/resource-detector-git/src/fecth-git-data.ts
@@ -0,0 +1,15 @@
+import child_process from 'child_process';
+import path from 'path';
+import fs from 'fs';
+
+export const executeGitCommand = (gitCommand: string): string => {
+    return child_process
+        .execSync(gitCommand, { stdio: ['ignore', 'pipe', 'pipe'] })
+        .toString()
+        .trim();
+};
+
+export const readFileFromGitDir = (pathInGit: string): string => {
+    const headFilePath = path.join(process.cwd(), '.git', pathInGit);
+    return fs.readFileSync(headFilePath).toString().trim();
+};
