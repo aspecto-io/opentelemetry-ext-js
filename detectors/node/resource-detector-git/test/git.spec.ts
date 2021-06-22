4b0570f2aa76b899b268871653e185973d795ad1
Amir Blum <amir@aspecto.io>
2021-06-22 11:01:35 +0300

    print in test for ci failure debug

--- detectors/node/resource-detector-git/test/git.spec.ts
+++ detectors/node/resource-detector-git/test/git.spec.ts
@@ -88,6 +88,7 @@ describe('service detector', () => {
     describe('git branch', () => {
         it('read with git cli', () => {
             const expectedBranchName = child_process.execSync('git branch --show-current').toString().trim();
+            console.log('===========', { expectedBranchName });
             const resource = gitSyncDetector.createGitResourceFromGitDb();
             expect(resource.attributes[GitResourceAttributes.VCS_BRANCH_NAME]).toMatch(expectedBranchName);
         });
