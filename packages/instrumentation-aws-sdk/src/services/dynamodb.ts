7f1a3e6ce416a7531b4baf3866a86c9a19968685
(Eliseo) Nathaniel Ruiz Nowell <nathanielruiz98@gmail.com>
2021-08-10 02:06:10 -0700

    feat: Add spec defined attributes for DynamoDB `BatchGetItem` (#166)

--- packages/instrumentation-aws-sdk/src/services/dynamodb.ts
+++ packages/instrumentation-aws-sdk/src/services/dynamodb.ts
@@ -1,21 +1,27 @@
-import { SpanKind } from '@opentelemetry/api';
+import { Span, SpanKind, Tracer } from '@opentelemetry/api';
 import { RequestMetadata, ServiceExtension } from './ServiceExtension';
 import { SemanticAttributes } from '@opentelemetry/semantic-conventions';
-import { NormalizedRequest } from '../types';
+import { AwsSdkInstrumentationConfig, NormalizedRequest, NormalizedResponse } from '../types';
 
 export class DynamodbServiceExtension implements ServiceExtension {
-    requestPreSpanHook(request: NormalizedRequest): RequestMetadata {
+    requestPreSpanHook(normalizedRequest: NormalizedRequest): RequestMetadata {
         let spanKind: SpanKind = SpanKind.CLIENT;
         let spanName: string;
         let isIncoming = false;
-        const operation = request.commandName;
+        const operation = normalizedRequest.commandName;
 
         const spanAttributes = {
             [SemanticAttributes.DB_SYSTEM]: 'dynamodb',
-            [SemanticAttributes.DB_NAME]: request.commandInput?.TableName,
+            [SemanticAttributes.DB_NAME]: normalizedRequest.commandInput?.TableName,
             [SemanticAttributes.DB_OPERATION]: operation,
-            [SemanticAttributes.DB_STATEMENT]: JSON.stringify(request.commandInput),
+            [SemanticAttributes.DB_STATEMENT]: JSON.stringify(normalizedRequest.commandInput),
         };
+
+        if (operation == 'BatchGetItem') {
+            spanAttributes[SemanticAttributes.AWS_DYNAMODB_TABLE_NAMES] = Object.keys(
+                normalizedRequest.commandInput.RequestItems
+            );
+        }
 
         return {
             isIncoming,
@@ -23,5 +29,18 @@ export class DynamodbServiceExtension implements ServiceExtension {
             spanKind,
             spanName,
         };
+    }
+
+    responseHook(response: NormalizedResponse, span: Span, tracer: Tracer, config: AwsSdkInstrumentationConfig) {
+        const operation = response.request.commandName;
+
+        if (operation === 'BatchGetItem') {
+            if ('ConsumedCapacity' in response.data) {
+                span.setAttribute(
+                    SemanticAttributes.AWS_DYNAMODB_CONSUMED_CAPACITY,
+                    response.data.ConsumedCapacity.map((x: { [DictionaryKey: string]: any }) => JSON.stringify(x))
+                );
+            }
+        }
     }
 }
