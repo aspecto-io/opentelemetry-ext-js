156b68490069d032797836c9e4a5e4d96dd78eb8
Motti Bechhofer <motti.bechhofer@smartbear.com>
2024-05-16 12:48:23 +0300

    update ts and skipLibCheck

--- packages/instrumentation-typeorm/src/utils/get-func-param-names.ts
+++ packages/instrumentation-typeorm/src/utils/get-func-param-names.ts
@@ -3,7 +3,9 @@ const ARGUMENT_NAMES = /([^\s,]+)/g;
 
 export function getParamNames(func: Function) {
     const fnStr = func.toString().replace(STRIP_COMMENTS, '');
-    let result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
-    if (result === null) result = [];
+    let result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES) as any;
+    if (result === null) {
+        result = [];
+    }
     return result;
 }
